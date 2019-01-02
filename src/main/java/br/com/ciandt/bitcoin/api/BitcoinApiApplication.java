7eee52866ffafd8f992d852b01c1add3ae470f1f
Igor Henrique Araujo <igorha@primeinf.com.br>
2019-01-02 19:12:22 -0200

    Adicionando projeto

---
+++ src/main/java/br/com/ciandt/bitcoin/api/BitcoinApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.ciandt.bitcoin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinApiApplication.class, args);
+	}
+
+}
+

e3b6f90bf8f88474c3e0465f5524a303b6e0ad7d
Igor Henrique Araujo <igorha@primeinf.com.br>
2019-01-02 22:25:50 -0200

    Novo teste de CI com Travis

--- src/main/java/br/com/ciandt/bitcoin/api/BitcoinApiApplication.java
+++ src/main/java/br/com/ciandt/bitcoin/api/BitcoinApiApplication.java
@@ -8,7 +8,6 @@ public class BitcoinApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BitcoinApiApplication.class, args);
-		System.out.println("Projeto Bitcoin");
 	}
 
 }

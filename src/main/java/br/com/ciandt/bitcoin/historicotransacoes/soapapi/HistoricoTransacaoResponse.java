76985ff72da561e4f48965b447a7fdab0ddc7777
Igor Henrique Araujo <igorha@primeinf.com.br>
2019-01-07 17:16:06 -0200

    Adição do Spring Actuator a aplicação

--- src/main/java/br/com/ciandt/bitcoin/historicotransacoes/soapapi/HistoricoTransacaoResponse.java
+++ src/main/java/br/com/ciandt/bitcoin/historicotransacoes/soapapi/HistoricoTransacaoResponse.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.11 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2019.01.07 às 02:29:28 PM BRST 
+// Gerado em: 2019.01.07 às 05:07:51 PM BRST 
 //
 
 
@@ -102,13 +102,5 @@ public class HistoricoTransacaoResponse {
         }
         return this.historicoTransacoes;
     }
-
-	@Override
-	public String toString() {
-		return "HistoricoTransacaoResponse [numeroCarteira=" + numeroCarteira + ", historicoTransacoes="
-				+ historicoTransacoes + "]";
-	}
-    
-    
 
 }
--- src/main/java/br/com/ciandt/bitcoin/historicotransacoes/soapapi/package-info.java
+++ src/main/java/br/com/ciandt/bitcoin/historicotransacoes/soapapi/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.11 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2019.01.07 às 02:29:28 PM BRST 
+// Gerado em: 2019.01.07 às 05:07:51 PM BRST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ciandt.com.br/xml/carteirabitcoin", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
